b736b9aead812ac3579ff0406cb19dea389745ba
Thang Tuấn Phát <Saladegg24@gmail.com>
2025-04-08 20:03:53 +0700

    removed dnd-kit imports

--- src/components/orders/orders-column.tsx
+++ src/components/orders/orders-column.tsx
@@ -1,14 +1,11 @@
 import { Order } from "@/src/app/type";
-import React, { useMemo } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Badge } from "../ui/badge";
 import OrderCard from "./order-card";
-import { SortableContext } from "@dnd-kit/sortable";
-import { type UniqueIdentifier } from "@dnd-kit/core";
 import { ScrollArea } from "../ui/scroll-area";
 
 export type Column = {
-  id: UniqueIdentifier;
+  id: string;
   title: string;
   status: "pending" | "in-progress" | "completed";
   headingColor: string;
@@ -29,10 +26,6 @@ interface OrderColumnProps {
 export default function OrderColumn({ column, cards }: OrderColumnProps) {
   const filteredCards = cards.filter((c) => c.status === column.status);
 
-  const orderIds = useMemo(() => {
-    return cards.map((card) => card.id);
-  }, [cards]);
-
   return (
     <Card className={`${column.headingColor} border-t-4 flex flex-auto h-full`}>
       <CardHeader className="pb-2 flex-shrink-0">
@@ -45,11 +38,9 @@ export default function OrderColumn({ column, cards }: OrderColumnProps) {
       </CardHeader>
       <ScrollArea className="flex-1 min-h-0">
         <CardContent className="w-full h-150">
-          <SortableContext items={orderIds}>
-            {filteredCards.map((c) => {
-              return <OrderCard key={c.id} task={c} />;
-            })}
-          </SortableContext>
+          {filteredCards.map((c) => {
+            return <OrderCard key={c.id} task={c} />;
+          })}
         </CardContent>
       </ScrollArea>
     </Card>
